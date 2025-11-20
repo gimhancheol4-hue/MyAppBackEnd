39ac289522bba37b1c66759f766680ded9c1bc3d
KimHancheol <gimhancheol4@gmail.com>
2025-11-20 10:21:49 +0900

    프론트와 통신예정~

--- src/main/java/com/example/Home/HomeController.java
+++ src/main/java/com/example/Home/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.Home;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public String health() {
-        return "ok";
+    public ResponseEntity<Map<String, String>> health() {
+        return ResponseEntity.ok(Map.of("message", "ok"));
     }
 }
